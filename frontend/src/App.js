58b43a40bcf760a919d4fdd787713fedc0a33911
Dusan Velimirovic <dusan.velimirovic@yahoo.com>
2023-07-04 13:04:01 +0200

    create outlet reusable components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,57 @@
+// Import Externall modules
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+
 // Import Internal Modules
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import LeftBar from "./components/Leftbar/Leftbar";
+import RightBar from "./components/Rightbar/Rightbar";
+
+
 
 function App() {
+
+  // This propertie Allow us to use reusable components
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+
+
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+          <Layout />
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
     {
       path: "/login",
       element: <Login />,
---
+++ frontend/src/components/Leftbar/Leftbar.jsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+import "./leftBar.scss";
+
+const Leftbar = () => {
+  return (
+    <div>leftBar</div>
+  )
+}
+
+export default Leftbar
