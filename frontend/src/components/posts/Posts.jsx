309ef06b8c24d387d1e09f1f435df5bf44fb8911
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-08-18 16:17:00 +0200

    show user post

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -4,6 +4,8 @@ import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 
 const Posts = ({userId}) => {
+
+  // Show only posts witch belong to current user
   const { isLoading, error, data } = useQuery(["posts"], () =>
     makeRequest.get("/posts?userId="+userId).then((res) => {
       return res.data;
