e3cf2d8e70fcdad70d4c2e9e8e1bad47d8c5dd23
DusanVelimirovic <dusan.velimirovic@yahoo.com>
2023-08-18 16:36:37 +0200

    change profile data

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -14,11 +14,11 @@ import { makeRequest } from "../../axios";
 import { useLocation } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/authContext";
-//import Update from "../../components/update/Update";
+import Update from "../../components/update/Update";
 import { useState } from "react";
 
 const Profile = () => {
-  //const [openUpdate, setOpenUpdate] = useState(false);
+  const [openUpdate, setOpenUpdate] = useState(false);
   const { currentUser } = useContext(AuthContext);
 
   // Parse user id from URL
@@ -109,9 +109,10 @@ const Profile = () => {
                     <span>{data.website}</span>
                   </div>
                 </div>
-         
-              {rIsLoading ? (
+                {rIsLoading ? (
                   "loading"
+                ) : userId === currentUser.id ? (
+                  <button onClick={() => setOpenUpdate(true)}>update</button>
                 ) : (
                   <button onClick={handleFollow}>
                     {relationshipData.includes(currentUser.id)
@@ -119,10 +120,6 @@ const Profile = () => {
                       : "Follow"}
                   </button>
                 )}
-        
-
-                
-
               </div>
               <div className="right">
                 <EmailOutlinedIcon />
@@ -133,12 +130,7 @@ const Profile = () => {
           </div>
         </>
       )}
-      {
-        /*
-                            {openUpdate && <Update setOpenUpdate={setOpenUpdate} user={data} />}
-        */
-      }
-    
+      {openUpdate && <Update setOpenUpdate={setOpenUpdate} user={data} />}
     </div>
   );
 };
